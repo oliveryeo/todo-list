d0f8f3a4a821582e5b38c526cf807b1326a08cf3
oliveryjy <yjyoliver@hotmail.com>
2023-11-22 16:37:07 +0800

    add functionality to highlight selected tab

--- src/screenController.js
+++ src/screenController.js
@@ -2,8 +2,7 @@ import todoController from './todoController.js';
 
 // screenController Module
 const screenController = (() => {
-
-
+  initializePage();
 
   return {
 
@@ -11,7 +10,23 @@ const screenController = (() => {
 })();
 
 function initializePage() {
-  
+  const allTasks = document.querySelector('#all-tasks');
+  const today = document.querySelector('#today');
+  const week = document.querySelector('#week');
+  const newProject = document.querySelector('#new-project');
+
+  const initializingItems = new Array(allTasks, today, week, newProject);
+  initializingItems.forEach(item => {
+    // Add .selected-tab class to current selected tab
+    item.addEventListener('click', () => {
+      // Remove .selected-tab class from previous selected tab
+      const prevSelectedTab = document.querySelector('.selected-tab');
+      if (prevSelectedTab) {
+        prevSelectedTab.classList.remove('selected-tab');
+      }
+      item.classList.add('selected-tab');
+    });
+  });
 }
 
 
