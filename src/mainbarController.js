4549354faed4731f43b3d2fb832aac73ee854823
oliveryjy <yjyoliver@hotmail.com>
2023-12-19 21:26:25 +0800

    Create a template function in mainbarController.js

--- src/mainbarController.js
+++ src/mainbarController.js
@@ -1,3 +1,7 @@
 import todoController from './todoController.js';
 import todoControllerTestUnit from './todoControllerTestUnit.js';
 
+// Create function that takes in the sidebar tab title and todo array
+const loadMainBar = (tabTitle, todoArray) => {
+
+};
