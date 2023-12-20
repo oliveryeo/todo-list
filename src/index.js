40d99e8b402021df5507942ad728853808edf9ab
Oliver Yeo <65693954+oliveryeo@users.noreply.github.com>
2023-12-20 01:54:40 +0000

    Refactor mainbar and sidebar into DOMController.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './styles.css';
-import sidebarController from './sidebarController.js'; // Since the module pattern is imported, the code in the module will run immediately.
+import DOMController from './DOMController.js'; // Since the module pattern is imported, the code in the module will run immediately.
 
-sidebarController.initializePage();
+DOMController.initializePage();
 
 // Adds a mock todo into the todoController
-sidebarController.addTodoTestUnit();
+DOMController.addTodoTestUnit();
 
--- src/mainbarController.js
+++
@@ -1,7 +0,0 @@
-import todoController from './todoController.js';
-import todoControllerTestUnit from './todoControllerTestUnit.js';
-
-// Create function that takes in the sidebar tab title and todo array
-const loadMainBar = (tabTitle, todoArray) => {
-
-};
