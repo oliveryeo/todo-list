5ded6e8a45195228622949b257af8c1865f2a7f0
oliveryjy <yjyoliver@hotmail.com>
2023-12-28 14:22:18 +0800

    Add function to load mainbar UI upon tab selection
    Populate the mainbarController Module Pattern in DOMController.js.
    loadMainbar() function will add an event listener to all the side tabs
    to extract the tabTitle as well as the todoArray for loading onto the
    mainbar UI.
    
    _loadMainbarUI() used as a helper function that handles the modification
    of the mainbar title and the creation of new todos onto the mainbar by
    taking in the tabtitle and todoArray as the input arguments.
    
    Modify todoControllerTestUnit.js to assess capability for loading more
    than 1 todo on the page.
    
    One possible challenge foreseen in the future is related to the use of
    if-else condition in the loadMainbar() function, which will create a lot
    of dependency due to its specificity. However, since "All tasks",
    "Today", "Week" tabTitles will remain static, this should not be an
    issue.

--- src/DOMController.js
+++ src/DOMController.js
@@ -1,5 +1,7 @@
 import todoController from './todoController.js';
 import todoControllerTestUnit from './todoControllerTestUnit.js';
+import { format } from "date-fns";
+
 /* 
   Tab styler module pattern 
 */
@@ -174,52 +176,74 @@ const projectController = (() => {
 */
 const mainbarController = (() => {
   const loadMainbar = () => {
-    // TODO: Extract the sidebar title and todoArray upon clicking
+    // Select all tab buttons -> extract title and array -> load mainbar UI
     const allTabButtons = document.querySelectorAll("#home > button, #projects > button");
+    allTabButtons.forEach(tabButton => {
+      tabButton.addEventListener('click', () => {
+        // Initiate relevant variables
+        const tabDataTitle = tabButton.dataset.title;
+        let todoArray;
+        // Conditions to sieve out todoArray from all tasks, today, next 7 days, or projects
+        if (tabDataTitle == 'All tasks') {
+          todoArray = todoController.extractAllTodos();
+        } else if (tabDataTitle == 'Today') {
+          todoArray = todoController.extractTodayTodos();
+        } else if (tabDataTitle == 'Week') {
+          todoArray = todoController.extractWeekTodos();
+        } else {
+          todoArray = todoController.extractProjectTodos(tabDataTitle);
+        };
+
+        _loadMainbarUI(tabDataTitle, todoArray);
+
+      });
+    })
   };
 
-  function _loadMainbarUI(tabTitle, todoArray) {
+  function _loadMainbarUI(tabDataTitle, todoArray) {
     // Select .main-panel-title-content class -> change text content to tabTitle AND set the data-title attribute to tabTitle as well
     const mainPanelTitle = document.querySelector("#main-panel-title-content");
-    mainPanelTitle.textContent = tabTitle;
+    mainPanelTitle.textContent = tabDataTitle;
     // Select #main-panel-content id:
     const mainPanelContent = document.querySelector("#main-panel-content");
       // Clear out ALL the todos on the page
-      mainPanelContent.textContent = "";
+    mainPanelContent.textContent = "";
       // Using the todoArray, create:
-        
-      todoArray.forEach(todo => {
+    todoArray.forEach(todo => {
         // A button with data-todo attribute of the todo title
-        const todoButton = document.createElement('button');
-        todoButton.dataset.title = todo.title;
+      const todoButton = document.createElement('button');
+      todoButton.dataset.title = todo.title;
 
         // An input of checkbox type
-        const checkbox = document.createElement('input');
-        checkbox.setAttribute('type', 'input');
+      const checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
 
         // A div containing the todo title as the content
-        const todoTitle = document.createElement('div');
-        todoTitle.textContent = todo.title;
+      const todoTitle = document.createElement('div');
+      todoTitle.textContent = todo.title;
         // A div with a class .todo-due-date, inside which contains:
           // An img with the triangle flag
           // A div containing the todo due date as the content
-        const todoDueDateContainer = document.createElement('div');
-        todoDueDateContainer.classList.add(".todo-due-date");
-        const dueFlagImg = document.createElement('img');
-        dueFlagImg.setAttribute("src", "./icons/flag-triangle.svg");
-        const todoDueDate = document.createElement('div');
-        todoDueDate.textContent = todo.dueDate;
-        todoDueDateContainer.appendChild(dueFlagImg);
-        todoDueDateContainer.appendChild(todoDueDate);
+      const todoDueDateContainer = document.createElement('div');
+      todoDueDateContainer.classList.add("todo-due-date");
+      const dueFlagImg = document.createElement('img');
+      dueFlagImg.setAttribute("src", "./icons/flag-triangle.svg");
+      const todoDueDate = document.createElement('div');
+      todoDueDate.textContent = format(todo.dueDate, "dd MMM");
+      todoDueDateContainer.appendChild(dueFlagImg);
+      todoDueDateContainer.appendChild(todoDueDate);
 
         // Joining everything together
-        todoButton.appendChild(checkbox);
-        todoButton.appendChild(todoTitle);
-        todoButton.appendChild(todoDueDateContainer);
-      })
-        
-        
+      todoButton.appendChild(checkbox);
+      todoButton.appendChild(todoTitle);
+      todoButton.appendChild(todoDueDateContainer);
+      mainPanelContent.appendChild(todoButton);
+    })    
   };
+  
+  return {
+    loadMainbar
+  }
 })();
 
 
@@ -231,6 +255,7 @@ const DOMControllerModule = (() => {
     todoCountLoader.loadTodayTasksCount();
     todoCountLoader.loadWeekTasksCount();
     projectController.loadNewProjectUI();
+    mainbarController.loadMainbar();
   };
 
   const addTodoTestUnit = () => {
--- src/todoControllerTestUnit.js
+++ src/todoControllerTestUnit.js
@@ -8,11 +8,19 @@ const todoControllerTestUnit = () => {
     if (project.projectName == "Project-X") {
       console.log("If this is printed, Project-X was created!");
       console.log("Creating a Project-X exercise todo now for testing!");
+      
+      // Create a project todo
       let title = "Controlled push-ups";
       let description = "Control the descent of the push-ups for each repetition";
-      let dueDate = "2023-12-18";
+      let dueDate = "2023-12-27";
       let priority = "high";
       project.createTodo(title, description, dueDate, priority);
+
+      let titleTwo = "Controlled dips";
+      let descriptionTwo = "Control the descent of the dips, then push all the way up, look straight ahead";
+      let dueDateTwo = "2023-12-28";
+      let priorityTwo = "high";
+      project.createTodo(titleTwo, descriptionTwo, dueDateTwo, priorityTwo);
     }
   });
 
@@ -20,7 +28,7 @@ const todoControllerTestUnit = () => {
     // Sieve out the project name
     if (project.projectName == "Project-X") {
       project.allTodos.forEach((todo) => {
-        console.log("If this is printed, todo was successfully created!");
+        console.log("If this is printed, a todo was successfully created!");
         console.log(todo);
       })
     }
